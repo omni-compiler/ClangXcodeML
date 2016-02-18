9f158103a2fd3950bffe09eff57593df31404f2c
Hideaki Nagamine <hnagamin@trans-nt.com>
2016-02-18 16:12:00 +0900

    [WIP] show <operator> element

--- src/DeclarationsVisitor.cpp
+++ src/DeclarationsVisitor.cpp
@@ -234,6 +234,83 @@ DeclarationsVisitor::WrapLabelChild(void) {
   };
 }
 
+static std::string OverloadedOperatorKindToString(OverloadedOperatorKind op, size_t param) {
+  const static std::map<OverloadedOperatorKind, std::string> unique_meaning = {
+    // 7.10 binary operators
+    // arithmetic binary operators
+    {OO_Slash,                "divExpr"},
+    {OO_Percent,              "modExpr"},
+    {OO_LessLess,             "LshiftExpr"},
+    {OO_GreaterGreater,       "RshiftExpr"},
+    {OO_Pipe,                 "bitOrExpr"},
+    {OO_Caret,                "bitXorExpr"},
+
+    // assignment operators
+    {OO_PlusEqual,            "asgPlusExpr"},
+    {OO_MinusEqual,           "asgMinusExpr"},
+    {OO_StarEqual,            "asgMulExpr"},
+    {OO_SlashEqual,           "asgDivExpr"},
+    {OO_PercentEqual,         "asgModExpr"},
+    {OO_LessLessEqual,        "asgLshiftExpr"},
+    {OO_GreaterGreaterEqual,  "asgRshiftExpr"},
+    {OO_AmpEqual,             "asgBitAndExpr"},
+    {OO_PipeEqual,            "asgBitOrExpr"},
+    {OO_CaretEqual,           "asgBitXorExpr"},
+
+    // logical binary operators
+    {OO_EqualEqual,           "logEQExpr"},
+    {OO_ExclaimEqual,         "logNEQExpr"},
+    {OO_GreaterEqual,         "logGEExpr"},
+    {OO_Greater,              "logGTExpr"},
+    {OO_LessEqual,            "logLEExpr"},
+    {OO_Less,                 "logLTExpr"},
+    {OO_AmpAmp,               "logAndExpr"},
+      // rvalue reference operator(&&) is neither an operator nor overloadable
+    {OO_PipePipe,             "logOrExpr"},
+    {OO_Equal,                "assignExpr"},
+
+    // 7.11 unary operators
+    {OO_Tilde,                "bitNotExpr"},
+    {OO_Exclaim,              "logNotExpr"},
+
+    // 7.13 commaExpr element
+    {OO_Comma,                "commaExpr"},
+
+    // 7.19 newExpr and newArrayExpr elements
+    // 7.20 deleteExpr and deleteArrayExpr elements
+    {OO_New,                  "newExpr"},
+    {OO_Array_New,            "newArrayExpr"},
+    {OO_Delete,               "deleteExpr"},
+    {OO_Array_Delete,         "deleteArrayExpr"},
+
+    // XXX: undocumented yet, should be discussed
+    {OO_Arrow,                "arrowExpr"},
+    {OO_ArrowStar,            "arrowStarExpr"},
+    {OO_Call,                 "callExpr"},
+    {OO_Subscript,            "subScriptExpr"}
+  };
+
+  switch (op) {
+    case OO_Plus:
+      return param == 1 ? "plusExpr" : "unaryPlusExpr";
+      // XXX: unray plus operator is not defined in document yet.
+      // See 7.11 unary operators.
+    case OO_Minus:
+      return param == 1 ? "minusExpr" : "unaryMinusExpr";
+    case OO_Star:
+      return param == 1 ? "mulExpr" : "pointerRef"/*XXX: correct name?*/;
+    case OO_Amp:
+      return param == 1 ? "logAndExpr" : "varAddr"/*XXX: correct name?*/;
+    case OO_PlusPlus:
+      return param == 1 ? "postIncrExpr" : "preIncrExpr";
+    case OO_MinusMinus:
+      return param == 1 ? "postDecrExpr" : "preIncrExpr";
+    default:
+      return unique_meaning.at(op);
+  }
+}
+
+
 #define NStmt(mes) do {newChild(mes); return true;} while (0)
 #define NStmtXXX(mes) NStmt("Stmt_" mes)
 #define NExpr(mes, content) do {                                        \
@@ -1106,6 +1183,68 @@ DeclarationsVisitor::PreVisitDecl(Decl *D) {
   case Decl::ObjCIvar: NDeclXXX("ObjCIvar");
   case Decl::Function:
   case Decl::CXXMethod:
+  {
+    // 5.1, 5.4 XXX
+    FunctionDecl *FD = static_cast<FunctionDecl*>(D);
+    size_t param(FD->param_size());
+    if (FD && FD->isThisDeclarationADefinition()) {
+      DeclarationName DN(FD->getDeclName());
+      newChild("functionDefinition");
+      newProp("access", getAccessAsString(D).c_str());
+      setLocation(FD->getLocStart());
+
+      xmlNodePtr functionNode = curNode;
+      HookForDeclarationNameInfo = [this, D, DN, param](DeclarationNameInfo NI) {
+        DeclarationsVisitor V(this);
+        DeclarationName::NameKind NK(DN.getNameKind());
+        if (NK == DeclarationName::CXXOperatorName ||
+            NK == DeclarationName::CXXLiteralOperatorName) {
+          newComment("DeclarationNameInfo_CXXOperatorName");
+          OverloadedOperatorKind op(DN.getCXXOverloadedOperator());
+          addChild("operator", OverloadedOperatorKindToString(op, param).c_str());
+          SymbolsVisitor SV(mangleContext, curNode, "symbols", typetableinfo);
+          SV.TraverseChildOfDecl(D);
+          addChild("params"); //create a new node to parent just after NameInfo
+          return true;
+        } else if (V.TraverseDeclarationNameInfo(NI)) {
+          SymbolsVisitor SV(mangleContext, curNode, "symbols", typetableinfo);
+          SV.TraverseChildOfDecl(D);
+          newChild("params"); //create a new node to parent just after NameInfo
+          return true;
+        } else {
+          return false;
+        }
+      };
+      bool IsValiadic = static_cast<FunctionDecl*>(D)->isVariadic();
+      HookForStmt = [this, functionNode, IsValiadic](Stmt *S) {
+        if (IsValiadic) {
+          addChild("ellipsis");
+        }
+        curNode = functionNode;
+        newChild("body");
+        return TraverseStmt(S);
+      };
+    } else {
+      DeclarationName DN(FD->getDeclName());
+      newChild("functionDecl");
+      newProp("access", getAccessAsString(D).c_str());
+      HookForDeclarationNameInfo = [this, D, DN, param](DeclarationNameInfo NI) {
+        DeclarationName::NameKind NK(DN.getNameKind());
+        if (NK == DeclarationName::CXXOperatorName ||
+            NK == DeclarationName::CXXLiteralOperatorName) {
+          newComment("DeclarationNameInfo_CXXOperatorName");
+          OverloadedOperatorKind op(DN.getCXXOverloadedOperator());
+          addChild("operator", OverloadedOperatorKindToString(op, param).c_str());
+        } else {
+          DeclarationsVisitor V(this);
+          V.TraverseDeclarationNameInfo(NI);
+        }
+        return false;
+      };
+    }
+
+    return true;
+  }
   case Decl::CXXConstructor:
   case Decl::CXXConversion:
   case Decl::CXXDestructor:
@@ -1274,75 +1413,6 @@ DeclarationsVisitor::PreVisitNestedNameSpecifierLoc(NestedNameSpecifierLoc NNS) 
 }
 #undef NNNSLocXXX
 
-static std::string OverloadedOperatorKindToString(OverloadedOperatorKind op) {
-  const static std::map<OverloadedOperatorKind, std::string> unique_meaning = {
-    // 7.10 binary operators
-    // arithmetic binary operators
-    {OO_Slash,                "divExpr"},
-    {OO_Percent,              "modExpr"},
-    {OO_LessLess,             "LshiftExpr"},
-    {OO_GreaterGreater,       "RshiftExpr"},
-    {OO_Pipe,                 "bitOrExpr"},
-    {OO_Caret,                "bitXorExpr"},
-
-    // assignment operators
-    {OO_PlusEqual,            "asgPlusExpr"},
-    {OO_MinusEqual,           "asgMinusExpr"},
-    {OO_StarEqual,            "asgMulExpr"},
-    {OO_SlashEqual,           "asgDivExpr"},
-    {OO_PercentEqual,         "asgModExpr"},
-    {OO_LessLessEqual,        "asgLshiftExpr"},
-    {OO_GreaterGreaterEqual,  "asgRshiftExpr"},
-    {OO_AmpEqual,             "asgBitAndExpr"},
-    {OO_PipeEqual,            "asgBitOrExpr"},
-    {OO_CaretEqual,           "asgBitXorExpr"},
-
-    // logical binary operators
-    {OO_EqualEqual,           "logEQExpr"},
-    {OO_ExclaimEqual,         "logNEQExpr"},
-    {OO_GreaterEqual,         "logGEExpr"},
-    {OO_Greater,              "logGTExpr"},
-    {OO_LessEqual,            "logLEExpr"},
-    {OO_Less,                 "logLTExpr"},
-    {OO_AmpAmp,               "logAndExpr"},
-      // rvalue reference operator(&&) is neither an operator nor overloadable
-    {OO_PipePipe,             "logOrExpr"},
-    {OO_Equal,                "assignExpr"},
-
-    // 7.11 unary operators
-    {OO_Tilde,                "bitNotExpr"},
-    {OO_Exclaim,              "logNotExpr"},
-
-    // 7.13 commaExpr element
-    {OO_Comma,                "commaExpr"},
-
-    // 7.19 newExpr and newArrayExpr elements
-    // 7.20 deleteExpr and deleteArrayExpr elements
-    {OO_New,                  "newExpr"},
-    {OO_Array_New,            "newArrayExpr"},
-    {OO_Delete,               "deleteExpr"},
-    {OO_Array_Delete,         "deleteArrayExpr"},
-
-    // XXX: undocumented yet, should be discussed
-    {OO_Arrow,                "arrowExpr"},
-    {OO_ArrowStar,            "arrowStarExpr"},
-    {OO_Call,                 "callExpr"},
-    {OO_Subscript,            "subScriptExpr"}
-  };
-
-  switch (op) {
-    case OO_Plus:
-    case OO_Minus:
-    case OO_Star:
-    case OO_Amp:
-    case OO_PlusPlus:
-    case OO_MinusMinus:
-    default:
-      return unique_meaning.at(op);
-  }
-}
-
-
 #define NDeclName(mes) do {                                     \
     newComment("DeclarationNameInfo_" mes);                     \
     newChild("name", content);                                  \
@@ -1370,11 +1440,7 @@ DeclarationsVisitor::PreVisitDeclarationNameInfo(DeclarationNameInfo NI) {
   case DeclarationName::ObjCZeroArgSelector: NDeclName("ObjCZeroArgSelector");
   case DeclarationName::ObjCOneArgSelector: NDeclName("ObjCOneArgSelector");
   case DeclarationName::ObjCMultiArgSelector: NDeclName("ObjCMultiArgSelector");
-  case DeclarationName::CXXOperatorName: {
-    newComment("DeclarationNameInfo_CXXDestructorName");
-    newChild("operator", OverloadedOperatorKindToString(DN.getCXXOverloadedOperator()).c_str());
-    return true;
-  }
+  case DeclarationName::CXXOperatorName: NDeclName("CXXOperatorName");
   case DeclarationName::CXXLiteralOperatorName: NDeclName("CXXLiteralOperatorName");
   case DeclarationName::CXXUsingDirective: NDeclName("CXXUsingDirective");
   }
