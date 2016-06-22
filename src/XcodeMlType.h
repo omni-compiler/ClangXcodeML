ded05e9d53dc728ddf3f94dd1814cea8d5e4b2a0
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-06-22 19:38:57 +0900

    Improve XcodeMlType
    Use pimpl & copy-and-swap to secure copy constructor.
    Implement destructor (XcodeMlType::Impl::~Impl) to free variant members
    absolutely.
    Rename XcodeMlTypeRef to RefXcodeMlType (industry best practice).
    Let XcodeMlType copyable.

--- src/XcodeMlType.h
+++ src/XcodeMlType.h
@@ -2,25 +2,25 @@
 #include <memory>
 #include <string>
 
-struct XcodeMlType;
-using RefXcodeMlType = std::unique_ptr<XcodeMlType>; /* not nullable */
+class XcodeMlType;
+using XcodeMlTypeRef = std::shared_ptr<XcodeMlType>; /* not nullable */
 
 struct XcodeMlReservedType {
   std::string name;
 };
 
 struct XcodeMlPointerType {
-  RefXcodeMlType ref;
+  XcodeMlTypeRef ref;
 };
 
 struct XcodeMlFunctionType {
-  RefXcodeMlType returnType;
-  std::vector<RefXcodeMlType> params;
+  XcodeMlTypeRef returnType;
+  std::vector<XcodeMlTypeRef> params;
 };
 
 struct XcodeMlArrayType {
-  RefXcodeMlType elementType;
-  std::unique_ptr<size_t> size;
+  XcodeMlTypeRef elementType;
+  std::shared_ptr<size_t> size;
 };
 
 enum XcodeMlTypeKind {
@@ -30,12 +30,54 @@ enum XcodeMlTypeKind {
   XTK_Array,
 };
 
-struct XcodeMlType {
-  XcodeMlTypeKind kind;
-  union type {
-    XcodeMlReservedType reserved;
-    XcodeMlPointerType pointer;
-    XcodeMlFunctionType function;
-    XcodeMlArrayType array;
-  };
+class XcodeMlType {
+public:
+  void swap(XcodeMlType&);
+  XcodeMlType& operator=(XcodeMlType);
+private:
+  class Impl;
+  std::unique_ptr<Impl> impl;
 };
+
+void XcodeMlType::swap(XcodeMlType& other) {
+  std::swap(this->impl, other.impl);
+}
+
+namespace std {
+  template<>
+  void swap(XcodeMlType& lhs, XcodeMlType& rhs) {
+    lhs.swap(rhs);
+  }
+}
+
+XcodeMlType& XcodeMlType::operator=(XcodeMlType rhs) {
+  rhs.swap(*this);
+  return *this;
+}
+
+class XcodeMlType::Impl {
+public:
+  XcodeMlTypeKind kind;
+  union {
+    XcodeMlReservedType reservedType;
+    XcodeMlPointerType pointerType;
+    XcodeMlFunctionType functionType;
+    XcodeMlArrayType arrayType;
+  };
+  ~Impl();
+};
+
+XcodeMlType::Impl::~Impl() {
+  switch (kind) {
+    case XTK_None:
+      break;
+    case XTK_Reserved:
+      reservedType.~XcodeMlReservedType();
+    case XTK_Pointer:
+      pointerType.~XcodeMlPointerType();
+    case XTK_Function:
+      functionType.~XcodeMlFunctionType();
+    case XTK_Array:
+      arrayType.~XcodeMlArrayType();
+  }
+}
