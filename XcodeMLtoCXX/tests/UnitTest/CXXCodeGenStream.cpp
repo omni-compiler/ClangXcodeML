b89bfb49a150fa159d02d3414ec012b6ba4d881a
Hideaki Nagamine <1995.hnagamin@gmail.com>
2017-02-21 13:31:13 +0900

    Add more tests to CXXCodeGenStream
    Add newline_output_test, space_output_test, char_output_test,
    and space_redundancy_test.

--- XcodeMLtoCXX/tests/UnitTest/CXXCodeGenStream.cpp
+++ XcodeMLtoCXX/tests/UnitTest/CXXCodeGenStream.cpp
@@ -2,6 +2,8 @@
 #include <boost/test/included/unit_test.hpp>
 #include <string>
 #include <sstream>
+#include <tuple>
+#include <utility>
 
 #include "CXXCodeGen.h"
 
@@ -24,6 +26,51 @@ BOOST_AUTO_TEST_CASE(simple_string_output_test) {
   BOOST_TEST_CHECKPOINT("Stream accepts a string");
   stream << "string";
   BOOST_CHECK(stream.str() == "string");
+}
+
+BOOST_AUTO_TEST_CASE(newline_output_test) {
+  BOOST_TEST_CHECKPOINT("Stream accepts a newline_t");
+  stream << cxxgen::newline;
+  BOOST_CHECK(stream.str() == "\n");
+}
+
+BOOST_AUTO_TEST_CASE(space_output_test) {
+  BOOST_TEST_CHECKPOINT("Stream accepts a space_t");
+  stream << cxxgen::space;
+  BOOST_CHECK(stream.str() == " ");
+}
+
+BOOST_AUTO_TEST_CASE(char_output_test) {
+  BOOST_TEST_CHECKPOINT("Stream accepts a char");
+  const auto chars = static_cast<std::string>(" 01aA;\n\t");
+  for (char c : chars) {
+    const std::string str = {c};
+    BOOST_TEST_MESSAGE("Checking '" + str + "'");
+    cxxgen::Stream model, learner;
+    model << str, learner << c;
+    BOOST_CHECK(model.str() == learner.str());
+  }
+}
+
+BOOST_AUTO_TEST_CASE(space_redundancy_test) {
+  BOOST_TEST_CHECKPOINT("Stream does not emit redundant space");
+
+  const std::vector<std::pair<char,std::string>> seps = {
+    { '\n', "newline"},
+    { ' ', "space"},
+    { '\t', "tab" },
+  };
+
+  for (auto&& sep : seps) {
+    BOOST_TEST_MESSAGE("Checking " + sep.second);
+    const char s = sep.first;
+    cxxgen::Stream model, learner;
+
+    model << s, learner << s;
+    learner << cxxgen::space;
+
+    BOOST_CHECK(model.str() == learner.str());
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
