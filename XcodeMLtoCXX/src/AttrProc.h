e0df19bc01f8028c2bc0ba8b0af9db1ea14f8a31
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-03 20:01:37 +0900

    Make member functions of AttrProc inline

--- XcodeMLtoCXX/src/AttrProc.h
+++ XcodeMLtoCXX/src/AttrProc.h
@@ -6,18 +6,53 @@ class AttrProc {
 public:
   using Procedure = std::function<ReturnT(xmlNodePtr, T...)>;
   AttrProc() = delete;
+
   AttrProc(
-      const std::string&,
-      std::function<ReturnT(const std::vector<ReturnT>&)>,
-      std::function<ReturnT()>,
-      std::initializer_list<std::tuple<std::string, Procedure>>);
+      const std::string& a,
+      std::function<ReturnT(const std::vector<ReturnT>&)> f,
+      std::function<ReturnT()> e,
+      std::initializer_list<std::tuple<std::string, Procedure>> m):
+    attr(a),
+    fold(f),
+    empty(e),
+    map(m)
+  {}
+
   AttrProc(
-      const std::string&,
-      std::function<ReturnT(const std::vector<ReturnT>&)>,
-      std::function<ReturnT()>,
-      std::map<std::string, Procedure>&&);
-  ReturnT walk(xmlNodePtr, T...) const;
-  std::vector<ReturnT> walkAll(xmlNodePtr, T...) const;
+      const std::string& a,
+      std::function<ReturnT(const std::vector<ReturnT>&)> f,
+      std::function<ReturnT()> e,
+      std::map<std::string, Procedure>&& m):
+    attr(a),
+    fold(f),
+    empty(e),
+    map(m)
+  {}
+
+  ReturnT walk(xmlNodePtr node, T... args) const {
+    assert(node && node->type == XML_ELEMENT_NODE);
+    XMLString prop(xmlGetProp(node, BAD_CAST attr.c_str()));
+    auto iter = map.find(prop);
+    if (iter != map.end()) {
+      return (iter->second)(node, args...);
+    }
+    return empty();
+  }
+
+  std::vector<ReturnT> walkAll(xmlNodePtr node, T... args) const {
+    if (!node) {
+      return {};
+    }
+    std::vector<ReturnT> ret;
+    for (xmlNodePtr cur = xmlFirstElementChild(node);
+         cur;
+         cur = xmlNextElementSibling(cur))
+    {
+      ret.push_back(walk(cur, args...));
+    }
+    return ret;
+  }
+
 private:
   std::string attr;
   std::function<ReturnT(const std::vector<ReturnT>&)> fold;
@@ -25,56 +60,6 @@ private:
   std::map<std::string, Procedure> map;
 };
 
-template<typename ReturnT, typename... T>
-AttrProc<ReturnT, T...>::AttrProc(
-    const std::string& attr_,
-    std::function<ReturnT(const std::vector<ReturnT>&)> fold_,
-    std::function<ReturnT()> empty_,
-    std::initializer_list<
-      std::tuple<std::string, AttrProc::Procedure>> map_):
-  attr(attr_),
-  fold(fold_),
-  empty(empty_),
-  map(map_)
-{}
-
-template<typename ReturnT, typename... T>
-AttrProc<ReturnT, T...>::AttrProc(
-    const std::string& attr_,
-    std::function<ReturnT(const std::vector<ReturnT>&)> fold_,
-    std::function<ReturnT()> empty_,
-    std::map<std::string, Procedure> && map_):
-  attr(attr_),
-  fold(fold_),
-  empty(empty_),
-  map(map_)
-{}
-
-template<typename ReturnT, typename... T>
-ReturnT AttrProc<ReturnT, T...>::walk(xmlNodePtr node, T... args) const {
-  assert(node && node->type == XML_ELEMENT_NODE);
-  XMLString prop(xmlGetProp(node, BAD_CAST attr.c_str()));
-  auto iter = map.find(prop);
-  if (iter != map.end()) {
-    return (iter->second)(node, args...);
-  }
-  return empty();
-}
-
-template<typename ReturnT, typename... T>
-std::vector<ReturnT> AttrProc<ReturnT, T...>::walkAll(xmlNodePtr node, T... args) const {
-  if (!node) {
-    return {};
-  }
-  std::vector<ReturnT> ret;
-  for (xmlNodePtr cur = xmlFirstElementChild(node);
-       cur;
-       cur = xmlNextElementSibling(cur))
-  {
-    ret.push_back(walk(cur, args...));
-  }
-  return ret;
-}
 
 template<typename... T>
 class AttrProc<void, T...> {
