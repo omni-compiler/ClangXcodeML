a18ce76fb233edcbab9bc105ab3b35ad6616736b
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-03 19:55:25 +0900

    Change template signature of AttrProc
    Allow AttrProc::walk and AttrProc::walkAll to return values to caller.

--- XcodeMLtoCXX/src/AttrProc.h
+++ XcodeMLtoCXX/src/AttrProc.h
@@ -1,60 +1,79 @@
 #ifndef ATTRPROC_H
 #define ATTRPROC_H
 
-template<typename... T>
+template<typename ReturnT, typename... T>
 class AttrProc {
 public:
-  using Procedure = std::function<void(xmlNodePtr, T...)>;
+  using Procedure = std::function<ReturnT(xmlNodePtr, T...)>;
   AttrProc() = delete;
   AttrProc(
       const std::string&,
+      std::function<ReturnT(const std::vector<ReturnT>&)>,
+      std::function<ReturnT()>,
       std::initializer_list<std::tuple<std::string, Procedure>>);
-  AttrProc(const std::string&, std::map<std::string, Procedure>&&);
-  void walk(xmlNodePtr, T...) const;
-  void walkAll(xmlNodePtr, T...) const;
+  AttrProc(
+      const std::string&,
+      std::function<ReturnT(const std::vector<ReturnT>&)>,
+      std::function<ReturnT()>,
+      std::map<std::string, Procedure>&&);
+  ReturnT walk(xmlNodePtr, T...) const;
+  std::vector<ReturnT> walkAll(xmlNodePtr, T...) const;
 private:
   std::string attr;
+  std::function<ReturnT(const std::vector<ReturnT>&)> fold;
+  std::function<ReturnT()> empty;
   std::map<std::string, Procedure> map;
 };
 
-template<typename... T>
-AttrProc<T...>::AttrProc(
+template<typename ReturnT, typename... T>
+AttrProc<ReturnT, T...>::AttrProc(
     const std::string& attr_,
+    std::function<ReturnT(const std::vector<ReturnT>&)> fold_,
+    std::function<ReturnT()> empty_,
     std::initializer_list<
       std::tuple<std::string, AttrProc::Procedure>> map_):
   attr(attr_),
+  fold(fold_),
+  empty(empty_),
   map(map_)
 {}
 
-template<typename... T>
-AttrProc<T...>::AttrProc(
+template<typename ReturnT, typename... T>
+AttrProc<ReturnT, T...>::AttrProc(
     const std::string& attr_,
+    std::function<ReturnT(const std::vector<ReturnT>&)> fold_,
+    std::function<ReturnT()> empty_,
     std::map<std::string, Procedure> && map_):
   attr(attr_),
+  fold(fold_),
+  empty(empty_),
   map(map_)
 {}
 
-template<typename... T>
-void AttrProc<T...>::walk(xmlNodePtr node, T... args) const {
+template<typename ReturnT, typename... T>
+ReturnT AttrProc<ReturnT, T...>::walk(xmlNodePtr node, T... args) const {
   assert(node && node->type == XML_ELEMENT_NODE);
   XMLString prop(xmlGetProp(node, BAD_CAST attr.c_str()));
   auto iter = map.find(prop);
   if (iter != map.end()) {
-    (iter->second)(node, args...);
+    return (iter->second)(node, args...);
   }
+  return empty();
 }
 
-template<typename... T>
-void AttrProc<T...>::walkAll(xmlNodePtr node, T... args) const {
+template<typename ReturnT, typename... T>
+std::vector<ReturnT> AttrProc<ReturnT, T...>::walkAll(xmlNodePtr node, T... args) const {
   if (!node) {
-    return;
+    return {};
   }
+  std::vector<ReturnT> ret;
   for (xmlNodePtr cur = xmlFirstElementChild(node);
        cur;
        cur = xmlNextElementSibling(cur))
   {
-    walk(cur, args...);
+    ret.push_back(walk(cur, args...));
   }
+  return ret;
 }
 
 #endif /* !ATTRPROC_H */
