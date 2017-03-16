670cc8d38cf462d18fe2dc21a88c1256ab56aeb8
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-16 14:59:24 +0900

    Add NnsTableInfo.h to CXXtoXML

---
+++ CXXtoXML/src/NnsTableInfo.h
@@ -0,0 +1,20 @@
+#ifndef NNSTABLEINFO_H
+#define NNSTABLEINFO_H
+
+class NnsTableInfo {
+public:
+  NnsTableInfo() = delete;
+  NnsTableInfo(const NnsTableInfo&) = delete;
+  NnsTableInfo(NnsTableInfo&&) = delete;
+  NnsTableInfo& operator =(const NnsTableInfo&&) = delete;
+  NnsTableInfo& operator =(NnsTableInfo&&) = delete;
+
+  explicit NnsTableInfo(clang::MangleContext*);
+  std::string getNnsName(clang::NestedNameSpecifier*);
+private:
+  int seqForOther;
+  clang::MangleContext* MC;
+  std::map<clang::NestedNameSpecifier*, std::string> mapForOtherNns;
+};
+
+#endif
