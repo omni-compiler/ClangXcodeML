aabfb35275a854011f57bb2afa0fda162ee8ca62
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-20 18:01:52 +0900

    Add XcodeMlNns to XcodeMLtoCXX

---
+++ XcodeMLtoCXX/src/XcodeMlNns.cpp
@@ -0,0 +1,57 @@
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include "llvm/ADT/Optional.h"
+#include "llvm/Support/Casting.h"
+#include "StringTree.h"
+#include "XcodeMlType.h"
+#include "XcodeMlEnvironment.h"
+
+#include "XcodeMlNns.h"
+
+using CXXCodeGen::makeTokenNode;
+
+namespace XcodeMl {
+
+Nns::Nns(NnsKind k, const NnsIdent& ni):
+  kind(k),
+  ident(ni)
+{}
+
+Nns::~Nns() = default;
+
+NnsKind
+Nns::getKind() const {
+  return kind;
+}
+
+ClassNns::ClassNns(const NnsIdent& ni, const DataTypeIdent& di):
+  Nns(NnsKind::Class, ni),
+  dtident(di)
+{}
+
+Nns*
+ClassNns::clone() const {
+  ClassNns *copy = new ClassNns(*this);
+  return copy;
+}
+
+CodeFragment
+ClassNns::makeDeclaration(
+    const Environment& env) const
+{
+  const auto T = env.at(dtident);
+  const auto classT = llvm::cast<XcodeMl::ClassType>(T.get());
+  assert(classT);
+  const auto name = classT->name();
+  assert(name.hasValue());
+  return (*name) + makeTokenNode("::");
+}
+
+bool
+ClassNns::classof(const Nns* N) {
+  return N->getKind() == NnsKind::Class;
+}
+
+} // namespace XcodeMl
---
+++ XcodeMLtoCXX/src/XcodeMlNns.h
@@ -0,0 +1,51 @@
+#ifndef XCODEMLNNS_H
+#define XCODEMLNNS_H
+
+namespace XcodeMl {
+
+class Nns;
+using NnsRef = std::shared_ptr<Nns>;
+
+using CodeFragment = CXXCodeGen::StringTreeRef;
+
+using DataTypeIdent = std::string;
+
+class Environment;
+
+using NnsIdent = std::string;
+
+enum class NnsKind {
+  /*! classNNS */
+  Class,
+};
+
+class Nns {
+public:
+  Nns(NnsKind, const NnsIdent&);
+  virtual ~Nns() = 0;
+  virtual Nns* clone() const = 0;
+  NnsKind getKind() const;
+  virtual CodeFragment makeDeclaration(const Environment&) const = 0;
+protected:
+  Nns(const Nns&) = default;
+private:
+  NnsKind kind;
+  NnsIdent ident;
+};
+
+class ClassNns : public Nns {
+public:
+  ClassNns(const NnsIdent&, const DataTypeIdent&);
+  ~ClassNns() override = default;
+  Nns* clone() const override;
+  virtual CodeFragment makeDeclaration(const Environment&) const override;
+  static bool classof(const Nns *);
+protected:
+  ClassNns(const ClassNns&) = default;
+private:
+  DataTypeIdent dtident;
+};
+
+}
+
+#endif /* !XCODEMLNNS_H */
