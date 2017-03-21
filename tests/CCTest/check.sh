16830a92fd025a4c43c4250a33f80c265c2149fd
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-21 19:35:36 +0900

    Use print_line() in print_csv()

--- tests/CCTest/check.sh
+++ tests/CCTest/check.sh
@@ -69,19 +69,8 @@ print_line() {
 print_csv() {
   while read -r line
   do
-    echo -n ${line}.cc,
-    make_with_preferences ${line}.xcodeml
-    if [ $? -gt 0 ]; then
-      echo "Error"
-    else
-      echo -n "OK,"
-      make_with_preferences ${line}.dst.cpp
-      if [ $? -gt 0 ]; then
-        echo "Error"
-      else
-        echo "OK"
-      fi
-    fi
+    echo -n "${line}.cc,"
+    examine ${line} | print_line
   done
 }
 
