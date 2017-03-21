3e32c560e8ae5ab39879e71e8cd20da73765cefd
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-03-21 19:34:41 +0900

    Add helper function examine() to check.sh

--- tests/CCTest/check.sh
+++ tests/CCTest/check.sh
@@ -9,6 +9,33 @@ make_with_preferences() {
     $target
 }
 
+examine() {
+  target=$1
+  if $(make_with_preferences "${target}.xcodeml") ; then
+    echo  # success
+  else
+    echo 'CXXtoXcodeML fail'
+    exit 1
+  fi
+
+  if $(make_with_preferences "${target}.dst.cpp") ; then
+    echo  # success
+  else
+    echo 'XcodeMLtoCXX fail'
+    exit 1
+  fi
+
+  if $(clang++ -o "${target}.out" "${target}.dst.cpp") ; then
+    echo  # success
+  else
+    echo 'Compilation fail'
+    exit 1
+  fi
+
+  echo $(./"${target}.out")
+
+}
+
 print_csv() {
   while read -r line
   do
