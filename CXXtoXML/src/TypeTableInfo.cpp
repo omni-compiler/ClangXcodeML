9dc7fc3fcb2a435a00565d042a550bda30d79e72
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-04-07 18:32:10 +0900

    Use makeNameNode in makeSymbolsNodeForRecordType

--- CXXtoXML/src/TypeTableInfo.cpp
+++ CXXtoXML/src/TypeTableInfo.cpp
@@ -247,15 +247,8 @@ makeSymbolsNodeForRecordType(
        * Some field does not have name.
        *  Example: `struct A { int : 0; }; // unnamed bit field`
        */
-      auto nameNode = xmlNewChild(
-          idNode,
-          nullptr,
-          BAD_CAST "name",
-          BAD_CAST fieldName->getName().data());
-      xmlNewProp(
-          nameNode,
-          BAD_CAST "name_kind",
-          BAD_CAST "name");
+      auto nameNode = makeNameNode(TTI, field);
+      xmlAddChild(idNode, nameNode);
     }
     xmlAddChild(symbolsNode, idNode);
   }
