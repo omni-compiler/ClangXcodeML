f5c356b59e7820c25ea94ea01e21389f1c400472
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-05-29 15:56:32 +0900

    Emit clangStmt[@class="DeclRefExpr"]/name

--- CXXtoXML/src/DeclarationsVisitor.cpp
+++ CXXtoXML/src/DeclarationsVisitor.cpp
@@ -119,7 +119,14 @@ DeclarationsVisitor::PreVisitStmt(Stmt *S) {
     if (const auto DRE = dyn_cast<clang::DeclRefExpr>(ME->getBase())) {
       const auto DN = DRE->getNameInfo().getName();
       newBoolProp("is_access_to_anon_record", DN.isEmpty());
+      auto nameNode = makeNameNode(*typetableinfo, DRE);
+      xmlAddChild(curNode, nameNode);
     }
+  }
+
+  if (auto DRE = dyn_cast<clang::DeclRefExpr>(S)) {
+    auto nameNode = makeNameNode(*typetableinfo, DRE);
+    xmlAddChild(curNode, nameNode);
   }
 
   if (auto CL = dyn_cast<CharacterLiteral>(S)) {
