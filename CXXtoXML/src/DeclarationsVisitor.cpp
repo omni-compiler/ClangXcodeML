b8d5ba70daa1e3afb2dcabccb04b0a33d2bafd26
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-07-12 21:54:37 +0900

    Wrap base name element of MemberExpr with `base` element

--- CXXtoXML/src/DeclarationsVisitor.cpp
+++ CXXtoXML/src/DeclarationsVisitor.cpp
@@ -123,8 +123,11 @@ DeclarationsVisitor::PreVisitStmt(Stmt *S) {
     if (const auto DRE = dyn_cast<clang::DeclRefExpr>(ME->getBase())) {
       const auto DN = DRE->getNameInfo().getName();
       newBoolProp("is_access_to_anon_record", DN.isEmpty());
-      auto nameNode = makeNameNode(*typetableinfo, DRE);
-      xmlAddChild(curNode, nameNode);
+
+      auto baseName = makeNameNode(*typetableinfo, DRE);
+      auto base = xmlNewNode(nullptr, BAD_CAST "base");
+      xmlAddChild(base, baseName);
+      xmlAddChild(curNode, base);
     }
   }
 
