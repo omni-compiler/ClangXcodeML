a9f001bb3c1b3ee7b7a8c67397daf6472be391b2
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-10-23 22:13:47 +0900

    Remove `getDeclNameFromNameNode` from CodeBuilder.cpp

--- XcodeMLtoCXX/src/CodeBuilder.cpp
+++ XcodeMLtoCXX/src/CodeBuilder.cpp
@@ -63,37 +63,6 @@ getNns(const XcodeMl::NnsMap &nnsTable, xmlNodePtr nameNode) {
 }
 
 XcodeMl::CodeFragment
-getDeclNameFromNameNode(xmlNodePtr nameNode,
-    const llvm::Optional<XcodeMl::DataTypeIdent> &dtident,
-    const SourceInfo &src) {
-  const auto kind = getName(nameNode);
-  if (kind == "name") {
-    return makeTokenNode(getContent(nameNode));
-  } else if (kind == "operator") {
-    using namespace XcodeMl;
-    const auto op = makeOpNode(nameNode);
-    return makeTokenNode("operator") + op;
-  } else if (getName(nameNode) == "conversion") {
-    const auto dtident = getProp(nameNode, "destination_type");
-    const auto returnT = src.typeTable.at(dtident);
-    return makeTokenNode("operator")
-        + returnT->makeDeclaration(makeVoidNode(), src.typeTable);
-  }
-  assert(dtident.hasValue());
-  const auto T = src.typeTable[*dtident];
-  const auto classT = llvm::cast<XcodeMl::ClassType>(T.get());
-  const auto className = classT->name();
-  assert(className.hasValue());
-
-  if (kind == "constructor") {
-    return *className;
-  }
-
-  assert(kind == "destructor");
-  return makeTokenNode("~") + (*className);
-}
-
-XcodeMl::CodeFragment
 getQualifiedNameFromNameNode(xmlNodePtr nameNode,
     const llvm::Optional<XcodeMl::DataTypeIdent> &dtident,
     const SourceInfo &src) {
