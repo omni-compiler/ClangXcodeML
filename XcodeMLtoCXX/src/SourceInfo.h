30889f173333b148334f0e3786d34ec4281d8aec
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-11-03 18:36:19 +0900

    Add constructor to `SourceInfo`
    Cherry-picked from branch 'feature/unique-name'.
    
    Conflicts:
    	XcodeMLtoCXX/src/SourceInfo.h
    
    Conflicts happened because I had added the `language` member to
    `SourceInfo`.
    
    Original diff:
    
        diff --git a/XcodeMLtoCXX/src/SourceInfo.h b/XcodeMLtoCXX/src/SourceInfo.h
        index 32d7e41..7f879f3 100644
        --- a/XcodeMLtoCXX/src/SourceInfo.h
        +++ b/XcodeMLtoCXX/src/SourceInfo.h
        @@ -1,12 +1,19 @@
         #ifndef SOURCEINFO_H
         #define SOURCEINFO_H
    
        +namespace XcodeMl {
        +class Environment;
        +}
        +
         /*!
          * \brief A pack of necessary information for generating
          * C++ source code.
          */
         class SourceInfo {
         public:
        +  explicit SourceInfo(xmlXPathContextPtr c,
        +      const XcodeMl::Environment &e,
        +      const XcodeMl::NnsMap &n);
           std::string getUniqueName();
    
           xmlXPathContextPtr ctxt;

--- XcodeMLtoCXX/src/SourceInfo.h
+++ XcodeMLtoCXX/src/SourceInfo.h
@@ -1,6 +1,10 @@
 #ifndef SOURCEINFO_H
 #define SOURCEINFO_H
 
+namespace XcodeMl {
+class Environment;
+} // namespace XcodeMl
+
 enum class Language {
   Invalid,
   C,
@@ -13,6 +17,9 @@ enum class Language {
  */
 class SourceInfo {
 public:
+  explicit SourceInfo(xmlXPathContextPtr c,
+      const XcodeMl::Environment &e,
+      const XcodeMl::NnsMap &n);
   std::string getUniqueName();
 
   xmlXPathContextPtr ctxt;
