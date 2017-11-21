accc4d9dd200474c397ed8b4558069dfb7e9eb87
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-11-21 23:00:00 +0900

    Add `make_unique` to Stream.cpp
    C++11 does not support `std::make_unique`.
    
    Meyers, S. (2015). Effective Modern C++ (Senju, J., Trans., p.138).
    Tokyo, Japan: O'Reilly Japan.

--- XcodeMLtoCXX/src/Stream.cpp
+++ XcodeMLtoCXX/src/Stream.cpp
@@ -1,8 +1,19 @@
 #include <cassert>
+#include <memory>
 #include <sstream>
 #include <string>
 
 #include "Stream.h"
+
+namespace {
+
+template <typename T, typename... Ts>
+std::unique_ptr<T>
+make_unique(Ts &&... params) {
+  return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
+}
+
+} // namespace
 
 namespace CXXCodeGen {
 
