58c8930e0129c19cdf6a36cbc533da6b9febc891
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2017-12-13 23:59:51 +0900

    Don't add test files yet
    Revert 2 commits.
    * commit:5d4fe428b53157e6bc284ac05789a3665c12a649
    * commit:5b13134c469b38cac5bef34f9ac74105d5bfc37e
    
    Function template is not yet available in XcodeMLtoCXX.
    
    --
    Revert "Add test file (multi_templ_param.src.cpp)"
    This reverts commit 5d4fe428b53157e6bc284ac05789a3665c12a649.
    
    Revert "Add test file (templ_add.src.cpp)"
    This reverts commit 5b13134c469b38cac5bef34f9ac74105d5bfc37e.

--- tests/Compilability/multi_templ_param.src.cpp
+++
@@ -1,5 +0,0 @@
-template <typename FuncT, typename ReturnT, typename ParamT1, typename ParamT2>
-ReturnT
-apply(FuncT func, ParamT1 x, ParamT2 y) {
-  return func(x, y);
-}
--- tests/Compilability/templ_add.src.cpp
+++
@@ -1,5 +0,0 @@
-template <typename paramT>
-paramT
-add(paramT x, paramT y) {
-  return x + y;
-}
