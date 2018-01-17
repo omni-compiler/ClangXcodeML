b6f2ef18d197b115bc21925106710d9197b9befe
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-01-17 19:04:38 +0900

    Add comments to StringTree.h

--- XcodeMLtoCXX/src/StringTree.h
+++ XcodeMLtoCXX/src/StringTree.h
@@ -20,6 +20,7 @@ public:
   virtual ~StringTree() = 0;
   virtual StringTree *clone() const = 0;
   virtual void flush(Stream &) const = 0;
+  /* Cast to CXXCodeGen::InnerNode. */
   virtual InnerNode *lift() const = 0;
   StringTreeKind getKind() const;
 
@@ -39,7 +40,10 @@ public:
   ~InnerNode() = default;
   StringTree *clone() const override;
   void flush(Stream &) const override;
+  /*! \brief Copy and return `this`. It shallowly copies children. */
   InnerNode *lift() const override;
+  /*! \brief (Shallowly) Copy the child-string-nodes of `other` to
+   * `this->children`. */
   void amend(const StringTreeRef &other);
 
 protected:
@@ -67,13 +71,42 @@ private:
 
 std::string to_string(const StringTreeRef &);
 
+/*! \brief Make and return a string-node that evaluates to the empty string
+ * ("").
+ */
 StringTreeRef makeVoidNode();
+
+/*!
+ * \brief Make and return a string-node that evaluates to a string
+ * containing single new line character ("\n").
+ */
 StringTreeRef makeNewLineNode();
+
+/*!
+ * \brief Make and return a `CXXCodeGen::InnerNode` object containing
+ * `nodes` as children.
+ */
 StringTreeRef makeInnerNode(const std::vector<StringTreeRef> &nodes);
+
+/*! \brief Make and return a `CXXCodeGen::TokenNode` object. */
 StringTreeRef makeTokenNode(const std::string &);
 
+/*!
+ * \brief Returns a string-node created by concatnating the string-nodes,
+ * separated by line break("\n").
+ */
 StringTreeRef insertNewLines(const std::vector<StringTreeRef> &);
+
+/*!
+ * \brief Returns a string-node created by concatnating the string-nodes,
+ * separated by empty line("\n\n").
+ */
 StringTreeRef separateByBlankLines(const std::vector<StringTreeRef> &);
+
+/*!
+ * \brief Returns a string-node created by concatnating the string-nodes,
+ * separated by the given separator.
+ */
 StringTreeRef join(const std::string &, const std::vector<StringTreeRef> &);
 StringTreeRef wrapWithParen(const StringTreeRef &);
 StringTreeRef wrapWithSquareBracket(const StringTreeRef &);
