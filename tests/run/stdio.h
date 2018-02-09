888e98f3c6322f52ebd952877278498c98d5c1c6
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-02-09 13:22:53 +0900

    Add __cplusplus extern C to tests/run/*.h

--- tests/run/stdio.h
+++ tests/run/stdio.h
@@ -1,2 +1,10 @@
-extern "C" int printf(const char *, ...);
-extern "C" int puts(char *);
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+int printf(const char *, ...);
+int puts(char *);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
--- tests/run/stdlib.h
+++ tests/run/stdlib.h
@@ -2,10 +2,15 @@
 #define __STDLIB_H_INCLUDED__
 #include <stddef.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif /* __cplusplus */
 
 void *malloc(size_t);
 void free(void *);
+
+#ifdef __cplusplus
 }
+#endif /* __cplusplus */
 
 #endif
