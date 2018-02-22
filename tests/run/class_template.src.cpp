be3132ee6cac15fc571fdb53defdbf1aaaf69cb5
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-02-22 10:44:30 +0900

    Add tests/run/class_template.src.cpp

---
+++ tests/run/class_template.src.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+template <typename T>
+class ClassA {
+public:
+  ClassA(T x) {
+    data = x;
+  }
+
+public:
+  T
+  get() {
+    return data;
+  }
+
+private:
+  T data;
+};
+
+int
+main() {
+  ClassA<int> obj1(100);
+  printf("%d\n", obj1.get());
+}
