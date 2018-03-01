6f68bf3064db34e4f87fc0a4e2ea37929905c744
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-01 13:45:53 +0900

    Update func_templ_expl_spec_and_nontempl.src.cpp

--- tests/run/func_templ_expl_spec_and_nontempl.src.cpp
+++ tests/run/func_templ_expl_spec_and_nontempl.src.cpp
@@ -2,25 +2,45 @@
 
 template <typename T>
 const char *
-func(T x) {
-  return "func<T>";
+func1(T x) {
+  return "func1<T>";
 }
 
 template <>
 const char *
-func(int x) {
-  return "func<int>";
+func1(int x) {
+  return "func1<int>";
 }
 
 const char *
-func(int x) {
-  return "func";
+func1(int x) {
+  return "func1";
+}
+
+template<typename T>
+const char *
+func2() {
+  return "func2<T>";
+}
+
+template<>
+const char *
+func2<int>() {
+  return "func2<int>";
+}
+
+const char *
+func2() {
+  return "func2";
 }
 
 int
 main() {
-  printf("%s\n", func('c'));
-  printf("%s\n", func<int>(10));
-  printf("%s\n", func(10));
+  printf("%s\n", func1('c'));
+  printf("%s\n", func1<int>(10));
+  printf("%s\n", func1(10));
+  printf("%s\n", func2<char>());
+  printf("%s\n", func2<int>());
+  printf("%s\n", func2());
   return 0;
 }
