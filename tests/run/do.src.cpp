0b7a0148590151d360ae55d6040925d007bd6d86
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-05 20:23:22 +0900

    Update tests/run/do.src.cpp
    Problem: XcodeMLtoCXX cannot emit well-formed program.
    
        // tests/run/do.src.cpp
        extern "C" { int printf(char const *, ...); };
        extern "C" { int puts(char *); };
        void func1() {
          int i = 1;
          int n = 0;
          do {
            n = n + i;
            (++i);
          } while (i <= 100);
          printf("%d\n", n);
        }
        void func2() {
          int j = 1;
          do printf("func2, %d\n", (j++)) while (j < 10);  // here
        }
        void func3() {
          int i = 0;
          do
            class A {
            public:
              A(int x) { printf("func3, %d\n", x * x); }
            };
          class A obj((++i)) while (i < 20);               // here
        }
        int main() {
          func1();
          func2();
          func3();
          return 0;
        }

--- tests/run/do.src.cpp
+++ tests/run/do.src.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int
-main() {
+void
+func1() {
   int i = 1;
   int n = 0;
   do {
@@ -9,5 +9,32 @@ main() {
     ++i;
   } while (i <= 100);
   printf("%d\n", n);
+}
+
+void
+func2() {
+  int j = 1;
+  do
+    printf("func2, %d\n", j++);
+  while (j < 10);
+}
+
+void
+func3() {
+  int i = 0;
+  do
+    struct A {
+      A(int x) {
+        printf("func3, %d\n", x * x);
+      }
+    } obj(++i);
+  while (i < 20);
+}
+
+int
+main() {
+  func1();
+  func2();
+  func3();
   return 0;
 }
