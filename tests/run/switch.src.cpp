e6540c775a6c4d9793e646e266b8d9e3852fa540
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-06 20:09:15 +0900

    Update tests/run/switch.src.cpp

--- tests/run/switch.src.cpp
+++ tests/run/switch.src.cpp
@@ -1,8 +1,20 @@
+#include <stdio.h>
+
 int
 main() {
-  int i = 10;
-  switch (0 + 1) {
-  case 0: i = 0; break;
-  case 1: i = 1; break;
+  int count = 0;
+  int sum = 0;
+  for (int i = 0; i < 20; ++i) {
+    switch (i % 5) {
+    case 0: ++count;
+    case 1: printf("i % 5 < 2\n"); break;
+    case 2: {
+      printf("i % 5 = 2\n");
+      int k = i * i;
+      sum += k;
+    }
+    default: printf("i % 5 > 2\n");
+    }
   }
+  printf("%d %d\n", count, sum);
 }
