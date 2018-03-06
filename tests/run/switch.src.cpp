e5e885ed8fa8b6b69e4f984332ece528cd5e40f3
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-06 20:15:57 +0900

    Fix tests/run/switch.src.cpp

--- tests/run/switch.src.cpp
+++ tests/run/switch.src.cpp
@@ -7,13 +7,13 @@ main() {
   for (int i = 0; i < 20; ++i) {
     switch (i % 5) {
     case 0: ++count;
-    case 1: printf("i % 5 < 2\n"); break;
+    case 1: printf("i mod 5 < 2\n"); break;
     case 2: {
-      printf("i % 5 = 2\n");
+      printf("i mod 5 = 2\n");
       int k = i * i;
       sum += k;
     }
-    default: printf("i % 5 > 2\n");
+    default: printf("i mod 5 > 2\n");
     }
   }
   printf("%d %d\n", count, sum);
