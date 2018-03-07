b6a048038c4f9afdad3ba4ac7afc02f51c357820
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-07 12:05:53 +0900

    Improve tests/run/cast.src.cpp
        void print_as_string(unsigned long address) {
          char const *s = char const address;
          printf("%s\n", s);
        }
        int main() {
          char const *s = "const char *";
          print_as_string(s);
        }

--- tests/run/cast.src.cpp
+++ tests/run/cast.src.cpp
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 
 void
@@ -6,8 +7,16 @@ print_as_pint(void *pv) {
   printf("%d\n", *pi);
 }
 
+void
+print_as_string(size_t address) {
+  const char *s = (const char*)address;
+  printf("%s\n", s);
+}
+
 int
 main() {
   int i = 100;
   print_as_pint(&i);
+  const char *s = "const char *";
+  print_as_string((size_t)s);
 }
