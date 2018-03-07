ce84a2ba3d3712ab0ae08b45dc4937d2e063c76a
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-07 14:06:01 +0900

    Update tests/run/cpp_cast.src.cpp
        // tests/run/cpp_cast.dst.cpp
        void print_as_c(class ClassA *pa) {
          class ClassC *pc = pa;
          if (pc) {
            { pc->print(); };
          } else {
            { printf("pc is null"); };
          };
        }

--- tests/run/cpp_cast.src.cpp
+++ tests/run/cpp_cast.src.cpp
@@ -6,6 +6,9 @@ public:
   print() {
     printf("%d\n", i);
   }
+  virtual void
+  f() {
+  }
   int i;
 };
 
@@ -18,13 +21,32 @@ public:
   double d;
 };
 
-class ClassC : public ClassA, public ClassB {};
+class ClassC : public ClassA, public ClassB {
+public:
+  void
+  print() {
+    printf("%c\n", c);
+  }
+  char c;
+};
+
+void
+print_as_c(ClassA *pa) {
+  ClassC *pc = dynamic_cast<ClassC *>(pa);
+  if (pc) {
+    pc->print();
+  } else {
+    printf("pc is null");
+  }
+}
 
 int
 main() {
   ClassC obj;
   obj.i = 100;
   obj.d = 3.14;
+  obj.c = 'c';
   static_cast<ClassA>(obj).print();
   static_cast<ClassB>(obj).print();
+  print_as_c(&obj);
 }
