8f6c72bfe9c6cdb18a97e80b5f0842fcd5261860
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-07 19:11:59 +0900

    Update tests/run/goto.src.cpp

--- tests/run/goto.src.cpp
+++ tests/run/goto.src.cpp
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+void
+test_multiple_labels() {
+  int i = 0;
+label0:
+label1:
+label2:
+label3:
+  if (i > 9) {
+    return;
+  }
+  ++i;
+  switch (i % 4) {
+    case 0:
+      printf("zero\n");
+      goto label0;
+    case 1:
+      printf("one\n");
+      goto label1;
+    case 2:
+      printf("two\n");
+      goto label2;
+    default:
+      printf("other\n");
+      goto label3;
+  }
+}
+
 int
 main() {
   int n = 25;
@@ -17,5 +44,6 @@ cond:
   }
 end:
   printf("END\n");
+  test_multiple_labels();
   return 0;
 }
