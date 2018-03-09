b724e7bb4b41417d8a527d09eeb7da25153df0b9
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-09 12:06:40 +0900

    Test base class initialization
    XcodeMLtoCXX fails to make constructor.dst.cpp, saying "Assertion
    `false' failed."
    
        // XcodeMLtoCXX/src/CodeBuilder.cpp
        XcodeMl::CodeFragment
        getCtorInitName(xmlNodePtr node, const XcodeMl::Environment &env) {
          const auto dataMember = getPropOrNull(node, "member");
          if (dataMember.hasValue()) {
            return makeTokenNode(*dataMember);
          }
          const auto base = getPropOrNull(node, "type");
          if (base.hasValue()) {
            const auto T = env[*base];
            const auto classT = llvm::cast<XcodeMl::ClassType>(T.get());
            const auto name = classT->name();
            assert(name.hasValue());
            return *name;
          }
    
          xmlDebugDumpNode(stderr, node, 0);
          assert(false);                          // here
        }

--- tests/run/constructor.src.cpp
+++ tests/run/constructor.src.cpp
@@ -2,6 +2,8 @@
 
 class ClassA {
 public:
+  ClassA() : num(42) {
+  }
   ClassA(int i, int j) : num(i * j) {
   }
   ClassA(int i, int j, int k);
@@ -11,10 +13,25 @@ public:
 ClassA::ClassA(int i, int j, int k) : num(i * j * k) {
 }
 
+class ClassB : public ClassA {
+public:
+  ClassB() : ClassA(), bnum(100) {
+    bnum += num;
+  }
+  ClassB(int i) : ClassA(i, i), bnum(i) {
+    bnum += num;
+  }
+  int bnum;
+};
+
 int
 main() {
   ClassA a1(10, 20);
   printf("%d\n", a1.num);
   ClassA a2(10, 20, 30);
   printf("%d\n", a2.num);
+  ClassB b1 = ClassB();
+  printf("%d\n", b1.bnum);
+  ClassB b2(100);
+  printf("%d\n", b2.bnum);
 }
