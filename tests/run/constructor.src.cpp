899ff4509f0b0ac3f7cd25cb4f10f7bcbeec0a4f
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-09 11:47:34 +0900

    Test out-of-line constructor definition

--- tests/run/constructor.src.cpp
+++ tests/run/constructor.src.cpp
@@ -4,11 +4,17 @@ class ClassA {
 public:
   ClassA(int i, int j) : num(i * j) {
   }
+  ClassA(int i, int j, int k);
   int num;
 };
 
+ClassA::ClassA(int i, int j, int k) : num(i * j * k) {
+}
+
 int
 main() {
-  ClassA a(10, 20);
-  printf("%d\n", a.num);
+  ClassA a1(10, 20);
+  printf("%d\n", a1.num);
+  ClassA a2(10, 20, 30);
+  printf("%d\n", a2.num);
 }
