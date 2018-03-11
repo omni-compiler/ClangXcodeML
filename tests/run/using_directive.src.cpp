bb7ef833a6cdfc8ac45e5e58b5866c7625bf08b3
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-11 12:04:42 +0900

    Update tests/run/using_directive.src.cpp

--- tests/run/using_directive.src.cpp
+++ tests/run/using_directive.src.cpp
@@ -2,15 +2,19 @@
 
 namespace NS1 {
 
+namespace NS2 {
+
 void
 func() {
   printf("NS1::func\n");
 }
 
+} // namespace NS2
+
 } // namespace NS1
 
 int
 main() {
-  using namespace NS1;
+  using namespace NS1::NS2;
   func();
 }
