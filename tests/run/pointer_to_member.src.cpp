7a81a352502a4c2409b63dec9304a897580a91e9
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-26 21:34:32 +0900

    Update tests/run/pointer_to_member.src.cpp

--- tests/run/pointer_to_member.src.cpp
+++ tests/run/pointer_to_member.src.cpp
@@ -4,10 +4,21 @@ struct ClassA {
   int member_i;
 };
 
+struct Outer {
+  struct Inner {
+    char member_c;
+  };
+};
+
 int
 main() {
   int ClassA::*mpi_a = &ClassA::member_i;
   ClassA obja;
   obja.member_i = 20;
   printf("%d\n", obja.*mpi_a);
+
+  char ::Outer::Inner::*mpc_oi = &Outer::Inner::member_c;
+  Outer::Inner objoi;
+  objoi.member_c = '#';
+  printf("%c\n", objoi.*mpc_oi);
 }
