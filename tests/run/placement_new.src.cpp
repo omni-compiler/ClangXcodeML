ef0eda26a0b9d34c93615531fb3b5113104a3271
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-27 01:02:03 +0900

    Upadte tests/run/placement_new.src.cpp

--- tests/run/placement_new.src.cpp
+++ tests/run/placement_new.src.cpp
@@ -2,20 +2,17 @@
 #include <stdio.h>
 
 struct ClassA {
+  ClassA() : member_i(4) {}
   int member_i;
 };
 
-void *operator new(size_t, int i, char c) {
-  ClassA *pa = new ClassA;
-  pa->member_i = i * i;
-  return pa;
+void *operator new(size_t size, int i, char c) {
+  return operator new(size);
 }
 
 int
 main() {
-  ClassA *pv1 = (ClassA *)(new (100, 'c') ClassA);
-  ClassA *pv2 = (ClassA *)(new (100, 'c') ClassA());
-  printf("%d %d\n", pv1->member_i, pv2->member_i);
-  delete pv1;
-  delete pv2;
+  ClassA *pv = (ClassA *)(new (100, 'c') ClassA);
+  printf("%d\n", pv->member_i);
+  delete pv;
 }
