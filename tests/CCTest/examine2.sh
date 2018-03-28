71f1d671fe2a64e8f9fe12b6e03e206e45cc07a2
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-03-28 17:46:56 +0900

    Add tests/CCTest/examine2.sh

---
+++ tests/CCTest/examine2.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+cxxflags_for_dst=""
+for i in reorder \
+	deprecated \
+	parentheses-equality \
+	unused-parameter \
+	unused-variable \
+	c++11-long-long \
+	bitfield-constant-conversion \
+	sign-compare \
+	unused-private-field
+do
+    cxxflags_for_dst="${cxxflags_for_dst} -Wno-"$i
+done
+
+
+abort() {
+  echo "$@"
+  exit 1
+}
+
+target="$1"
+
+if grep -sq \^${target}\$ ignore.txt ; then
+  exit 0
+fi
+
+echo -n "${target}.cc,"
+
+make "${target}.xcodeml" >&2 || abort 'CXXtoXcodeML failed'
+make "${target}.dst.cpp" >&2 || abort 'XcodeMLtoCXX failed'
+
+echo -n 'OK,' # CXX -> XML conversion was correctly done
+
+clang++ ${cxxflags_for_dst} -o "${target}.out" "${target}.dst.cpp" || abort 'Compilation failed'
+
+echo -n 'OK, ' # XML -> C++ conversion was correctly done
+
+echo $(./"${target}.out")
